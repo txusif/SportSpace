ff918e2606f5e6757138b2663ac50b34cc27c84b
Txusif <txusif@gmail.com>
2024-09-17 13:31:33 +0530

    del bookindId, added sharp

--- app/api/turfs/[turfId]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function TurfApi() {
-  return <div>page</div>;
-}
-    
--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
       },
     ],
   },
-  output: "export",
+  // output: "export",
 };
 
 export default nextConfig;
